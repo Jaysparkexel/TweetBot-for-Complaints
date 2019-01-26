6f06eecc94d05343a39bdb84a2178946900c989f
Jay Vaghasiya <jayvaghasiya1@gmail.com>
2019-01-26 13:57:01 -0700

    Update updateStatus.js

--- Twitter/updateStatus.js
+++ Twitter/updateStatus.js
@@ -1,17 +1,17 @@
 // var Twitter = require('twitter')
 // var client = new Twitter({
-//     consumer_key: 'N6YAdXYnoZ98eTNcqN1wvLOJC',
-//     consumer_secret: 'D2yRfFht0nmgvBsjMKed2D4gaH7HC2WTq5c7wO14pxInBgJCeZ',
-//     access_token_key: '815869648562360320-NbeUdDzi47oZRQy4VLfD5E4VbkXstvM',
-//     access_token_secret: 'Kz8T3Fnoy1SnaLDblaEiC6mbGwXSl8lmw9xlzl8zDHb7Y'
+//     consumer_key: '',
+//     consumer_secret: '',
+//     access_token_key: '',
+//     access_token_secret: ''
 // });
 
 // var Twit = require('twit')
 // var T = new Twit({
-//     consumer_key:         'N6YAdXYnoZ98eTNcqN1wvLOJC',
-//     consumer_secret:      'D2yRfFht0nmgvBsjMKed2D4gaH7HC2WTq5c7wO14pxInBgJCeZ',
-//     access_token:         '815869648562360320-NbeUdDzi47oZRQy4VLfD5E4VbkXstvM',
-//     access_token_secret:  'Kz8T3Fnoy1SnaLDblaEiC6mbGwXSl8lmw9xlzl8zDHb7Y'
+//     consumer_key:         '',
+//     consumer_secret:      '',
+//     access_token:         '',
+//     access_token_secret:  ''
 // })
 
 module.exports = function (T, status, _callback) {
